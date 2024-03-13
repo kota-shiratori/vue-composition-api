ef7012d88e1f46389df8fafa7e756aa10112fc0b
kota_shiratori <swan.kouta@gmail.com>
2024-03-14 00:15:48 +0900

    🎨 CompositionAPIによるv-html,v-text,ref()

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup>
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
 
-const answer = computed(() => {
-  return 1 + 1
+// const answer = computed(() => {
+//   return 1 + 1
+// })
+
+const message = computed(() => {
+  return 'Hello <strong>' + myName.value  + '</strong>さん'
 })
+const welcomeColor = 'blue'
+const myName = ref('')
 </script>
 
 <template>
   <h1>Vue3</h1>
-  <p>1 + 1 = {{ answer }}</p>
+  <!-- <p>1 + 1 = {{ answer }}</p> -->
+  <p>お名前は？：<input type="text" size="30" v-model="myName"></p>
+  <p v-html="message" :style="{ 'background-color': welcomeColor, color: 'white' }"></p>
+  <p v-text="message"></p>
 </template>
