a33873cfc8d6163d0e7c540baa67fd25fd030aa8
kota_shiratori <swan.kouta@gmail.com>
2024-03-14 23:23:55 +0900

    🎨 CompositionAPIでのemit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppHeader from './components/AppHeader.vue';
+import TodoList from './components/TodoList.vue';
 import { ref } from 'vue';
 
 const todos = ref([])
@@ -21,9 +22,5 @@ const removeTodo = (index) => {
   <AppHeader color="blue">SLOT</AppHeader>
   <input type="text" size="30" v-model="newTodo">
   <button @click="addTodo">追加</button>
-  <ul v-if="todos.length > 0">
-    <li v-for="(todo, i) in todos" v-bind:key="i">{{ todo }} <span @click="removeTodo(i)"
-        style="cursor:pointer">X</span></li>
-  </ul>
-  <p v-else>※ Todoを追加してください</p>
+  <TodoList :todos="todos" @removeTodo="removeTodo"/>
 </template>
