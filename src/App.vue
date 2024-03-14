2945dae1d4d4a25b7d85c7864e770ba703c2a2c5
kota_shiratori <swan.kouta@gmail.com>
2024-03-14 22:22:37 +0900

    🎨 CompositionAPIでのイベント

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <script setup>
-import { ref, computed } from 'vue';
-
-// const answer = computed(() => {
-//   return 1 + 1
-// })
-
-const message = computed(() => {
-  return 'Hello <strong>' + myName.value  + '</strong>さん'
-})
-const welcomeColor = 'blue'
-const myName = ref('')
+import { ref } from 'vue'
+const number = ref(0)
+const add = () => {
+  number.value++
+}
 </script>
 
 <template>
-  <h1>Vue3</h1>
-  <!-- <p>1 + 1 = {{ answer }}</p> -->
-  <p>お名前は？：<input type="text" size="30" v-model="myName"></p>
-  <p v-html="message" :style="{ 'background-color': welcomeColor, color: 'white' }"></p>
-  <p v-text="message"></p>
+  <input type="text" size="30" v-model="number"> <button @click="add">+1</button>
 </template>
